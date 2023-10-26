46cf7353bd0454c9de106000a7135385b78e74f2
fjfj10 <kimmisuk2218@gmail.com>
2023-10-26 10:52:59 +0900

    feat: 게시글 검색기능 추가

--- src/pages/BoardList/BoardList.jsx
+++ src/pages/BoardList/BoardList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import RootContainer from '../../components/RootContainer/RootContainer';
 import { css } from '@emotion/react';
 import ReactSelect from 'react-select';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { instance } from '../../api/config/instance';
 import { useQuery } from 'react-query';
@@ -22,23 +23,22 @@ const table = css`
     & td {
         cursor: pointer;
     }
-`
+`;
 
 const searchContainer = css`
     display: flex;
     justify-content: flex-end;
     margin-bottom: 10px;
     width: 100%;
-
+    
     & > * {
         margin-left: 5px;
     }
-`
+`;
 
 const selectBox = css`
     width: 100px;
-
-`
+`;
 
 const SPageNumbers = css`
     display: flex;
@@ -47,7 +47,6 @@ const SPageNumbers = css`
     margin-top: 10px;
     width: 200px;
     list-style-type: none;
-
     & a {
         text-decoration: none;
         color: black;
@@ -60,10 +59,11 @@ const SPageNumbers = css`
         width: 20px;
         border: 1px solid #dbdbdb;
     }
-`
+
+`;
 
 function BoardList(props) {
-
+    const navigate = useNavigate();
     const { category, page } = useParams();
 
     const options = [
@@ -72,17 +72,40 @@ function BoardList(props) {
         {value: "작성자", label: "작성자"}
     ];
 
-    const [ boardList, setBoardList ] = useState();
+    const [ selectedOption, setSelectedOption ] = useState(options[0]);
+
+    const search = {
+        optionName: selectedOption.label,
+        searchValue: ""
+    }
+
+    const [ searchParams, setSearchParams ] = useState(search);
 
     const getBoardList = useQuery(["getBoardList", page, category], async () => {
         const option = {
-            params: {
-                optionName: "",
-                searchValue: ""
-            }
+            params: searchParams
         }
         return await instance.get(`/boards/${category}/${page}`, option);
-    })
+    });
+
+    const handleSearchInputChange = (e) => {
+        setSearchParams({
+            ...searchParams,
+            searchValue: e.target.value
+        })
+    }
+
+    const handleSearchOptionSelect = (option) => {
+        setSearchParams({
+            ...searchParams,
+            optionName: option.label
+        })
+    }
+
+    const handleSearchButtonClick = () => {
+        navigate(`/board/${category}/1`);
+        getBoardList.refetch();
+    }
 
     return (
         <RootContainer>
@@ -91,16 +114,17 @@ function BoardList(props) {
                 
                 <div css={searchContainer}>
                     <div css={selectBox}>
-                        <ReactSelect options={options} defaultInputValue={options[0]} />
+                        <ReactSelect options={options} defaultValue={options[0]} 
+                        onChange={handleSearchOptionSelect} />
                     </div>
-                    <input type="text" />
-                    <button>검색</button>
+                    <input type="text" onChange={handleSearchInputChange} />
+                    <button onClick={handleSearchButtonClick}>검색</button>
                 </div>
                 <table css={table}>
                     <thead>
                         <tr>
                             <th>번호</th>
-                            <th>게시글</th>
+                            <th>제목</th>
                             <th>작성자</th>
                             <th>작성일</th>
                             <th>추천</th>
@@ -109,18 +133,18 @@ function BoardList(props) {
                     </thead>
                     <tbody>
                         {!getBoardList.isLoading && getBoardList?.data?.data.map(board => {
-                            return <tr>
+                            return <tr key={board.boardId}>
                                         <td>{board.boardId}</td>
                                         <td>{board.title}</td>
                                         <td>{board.nickname}</td>
                                         <td>{board.createDate}</td>
-                                        <td>{board.likeCount}</td>
                                         <td>{board.hitsCount}</td>
+                                        <td>{board.likeCount}</td>
                                     </tr>
                         })}
-                        
                     </tbody>
                 </table>
+
                 <ul css={SPageNumbers}>
                     <Link to={`/board/${category}/${parseInt(page) - 1}`}><li>&#60;</li></Link>
                     <Link to={`/board/${category}/${1}`}><li>1</li></Link>
